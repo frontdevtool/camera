a48544109317cc08831fb7091f4c02972ca52871
frontdevtool <kazem84@hotmail.com>
2025-08-28 06:38:51 +0100

    start

---
+++ app/index.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+
+import "./global.css";
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+const index = () => {
+  return (
+  
+  
+  <View className="flex-1 justify-center items-center">
+    <Text className="text-3xl"> index</Text>
+  
+
+  </View>
+   
+  );
+};
+
+
+
+export default index;
